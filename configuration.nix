75e4af803f866c11abe09dee63200127aa68b351
Simon Voordouw <simonvoordouw@gmail.com>
2021-03-12 05:48:53 +0100

    automatically update every day

--- configuration.nix
+++ configuration.nix
@@ -166,6 +166,14 @@
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
+  system.autoUpgrade = {
+    enable = true;
+    flake = "/home/simon/src/dotfiles";
+    flags = [
+      "--update-input" "nixpkgs" "--commit-lock-file"
+    ];
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
