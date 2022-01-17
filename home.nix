4e46b2c4ada0571e7a63a29878f9e2da68b35522
Simon Voordouw <simon.voordouw@teamrockstars.nl>
2022-01-17 17:17:53 +0100

    cleanup home.nix

--- home.nix
+++ home.nix
@@ -8,9 +8,6 @@
       flavor = "gmail.com";
       primary = true;
     };
-  #   rockstars = {
-  #     address = "simon.voordouw@teamrockstars.nl";
-  #   };
   };
 
   home.packages = with pkgs; [
@@ -42,32 +39,18 @@
     bash
     curl
     helix
+    (python3Packages.python-lsp-server.override (old: {
+      withRope = true;
+      withFlake8 = true;
+    }))
+    python3Packages.python-lsp-black
+    python3Packages.pylsp-mypy
     rust-analyzer
   ];
 
   xdg.enable = true;
 
-  # fonts.fontconfig.enable = true;
-
-  # gtk = {
-  #   enable = true;
-  #   theme = {
-  #     name = "Adwaita";
-  #     package = pkgs.gnome3.gnome_themes_standard;
-  #   };
-  # };
-
-  programs.firefox = {
-    enable = true;
-    # package = pkgs.firefox-wayland;
-    # package = pkgs.wrapFirefox pkgs.firefox-unwrapped {
-    #   forceWayland = true;
-    #   extraPolicies = {
-    #     ExtensionSettings = { };
-    #   };
-    # };
-  };
-
+  programs.firefox.enable = true;
   programs.chromium.enable = true;
 
   programs.neovim = {
@@ -149,25 +132,17 @@
 
     extraConfig = "lua require('init')";
   };
-  # xdg.configFile."nvim/parser/c.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-c}/parser";
-  # xdg.configFile."nvim/parser/rust.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-rust}/parser";
-  # xdg.configFile."nvim/parser/markdown.so".source = "${pkgs.tree-sitter.builtGrammars.tree-sitter-markdown}/parser";
-  #
   xdg.configFile."nvim/lua".source = nvim/lua;
 
-  xdg.configFile."helix/config.toml".text = "theme = \"bogster\"";
-  programs.git = {
-    enable = true;
-    userEmail = "simonvoordouw@gmail.com";
-    userName = "Simon Voordouw";
-    extraConfig = {
-      pull.rebase = "false";
-      help.autocorrect = 10;
-    };
-  };
+  xdg.configFile."helix/config.toml".text = ''
+    theme = "bogster"
+  '';
 
   programs.fish = {
     enable = true;
+    interactiveShellInit = ''
+      any-nix-shell fish --info-right | source
+    '';
     functions = {
       bwu = "set -xU BW_SESSION (bw unlock --raw $argv[1])";
     };
@@ -178,8 +153,6 @@
     nix-direnv = {
       enable = true;
     };
-    enableFishIntegration = true;
-    enableBashIntegration = true;
   };
 
   programs.fzf = {
@@ -190,24 +163,5 @@
 
   home.sessionVariables = {
     EDITOR = "nvim";
-    # MOZ_ENABLE_WAYLAND = 1;
-    DOTNET_ROOT = "${pkgs.dotnet-sdk}";
-    ZK_NOTEBOOK_DIR = "/home/simon/notes";
-    # XCURSOR_PATH = lib.mkForce [ "${pkgs.gnome3.adwaita-icon-theme}/share/icons" ];
   };
-
-  programs.taskwarrior = {
-    enable = true;
-    extraConfig = ''
-      taskd.certificate=/home/simon/.config/taskwarrior/private.certificate.pem
-      taskd.key=/home/simon/.config/taskwarrior/private.key.pem
-      taskd.ca=/home/simon/.config/taskwarrior/ca.cert.pem
-      taskd.server=inthe.am:53589
-      taskd.credentials=inthe_am/simonvoordouw/3a2bfbe5-91a9-4a8f-8d8f-7404bb8f5fa6
-      taskd.trust=ignore hostname
-    '';
-  };
-  xdg.configFile."taskwarrior/private.certificate.pem".source = ./taskwarrior/private.certificate.pem;
-  xdg.configFile."taskwarrior/private.key.pem".source = ./taskwarrior/private.key.pem;
-  xdg.configFile."taskwarrior/ca.cert.pem".source = ./taskwarrior/ca.cert.pem;
 }
